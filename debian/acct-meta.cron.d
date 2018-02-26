d23d610d89a4ea44af94de0c235ac43738798848
root <root@minos1.zcu.cz>
2018-02-26 22:24:48 +0100

    Zkraceni cronu, vynechani testu na nenalezeny soubor.

--- debian/acct-meta.cron.d
+++ debian/acct-meta.cron.d
@@ -1,6 +1,8 @@
 #
 # Regular cron jobs for the acct-meta package
 #
+# Must start before midnight (when atop creates new log file)
+#
 #MAILTO=svamberg@civ.zcu.cz
-15 7	* * *	root	[ -x /usr/bin/acct-meta ] && /bin/bash -c 'sleep `expr $RANDOM \* 10 \% 72000`' && /usr/bin/acct-meta
+15 7	* * *	root	[ -x /usr/bin/acct-meta ] && /bin/bash -c 'sleep `expr $RANDOM \* 10 \% 57600`' && /usr/bin/acct-meta
 
