6c79589358d7ce6368a466613a0ba3b476cdffb7
root <root@tarkil1.grid.cesnet.cz>
2018-09-25 10:09:12 +0200

    Pridan PATH do cron.d skriptu, release 0.2.4

--- debian/acct-meta.cron.d
+++ debian/acct-meta.cron.d
@@ -4,5 +4,6 @@
 # Must start before midnight (when atop creates new log file)
 #
 #MAILTO=svamberg@civ.zcu.cz
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin:/usr/sbin
 15 7	* * *	root	[ -x /usr/bin/acct-meta ] && /bin/bash -c 'sleep `expr $RANDOM \* 10 \% 57600`' && /usr/bin/acct-meta
 
